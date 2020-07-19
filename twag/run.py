e195035ae500cafa3eec4e382cfc8829140f2c7f
AlexLuka <alexbabyboomer@gmail.com>
2020-07-19 15:32:21 -0500

    Added basic logger to the run script

--- twag/run.py
+++ twag/run.py
@@ -1,11 +1,24 @@
+import sys
+import logging
 from time import sleep
 
 
 def main():
     while True:
-        print("Running twag")
+        # print("Running twag")
+        logger.info("Running twag")
         sleep(10)
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger("twag")
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s] %(message)s")
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
     main()
